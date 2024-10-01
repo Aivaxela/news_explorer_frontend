e756f783c106e95908269e2b5b776032d5da1249
Riley Marcum <rmarcum13@gmail.com>
2024-09-30 20:26:35 -0400

    inital directory creation

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../blocks/Preloader.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <div className="circle-preloader"></div>
+    </div>
+  );
+}
+
+export default App;
