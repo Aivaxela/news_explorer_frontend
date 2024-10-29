86e803ec0e448547af6f69f872f009f44463217c
Riley Marcum <rmarcum13@gmail.com>
2024-10-29 19:31:36 -0400

    refactor api and auth methods

--- src/utils/api.js
+++ src/utils/api.js
@@ -9,70 +9,65 @@ export default class Api {
 
   getNewsArticles(query) {
     if (query) {
-      return fetch(`${this._baseUrl}/articles/search/${query}`, {
+      return this._request(`${this._baseUrl}/articles/search/${query}`, {
         method: "GET",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      }).then((res) => this._checkResponse(res));
+      });
     }
   }
 
-  getUser = (token) => {
-    return fetch(`${this._baseUrl}/users/`, {
+  getUser(token) {
+    return this._request(`${this._baseUrl}/users/`, {
       method: "GET",
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
       },
-    }).then((res) => this._checkResponse(res));
-  };
+    });
+  }
 
-  getArticles = (token) => {
-    return fetch(`${this._baseUrl}/articles/`, {
+  getArticles(token) {
+    return this._request(`${this._baseUrl}/articles/`, {
       method: "GET",
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
       },
-    }).then((res) => this._checkResponse(res));
-  };
+    });
+  }
 
-  saveArticle = (article, token) => {
-    return fetch(`${this._baseUrl}/articles/`, {
+  saveArticle(article, token) {
+    return this._request(`${this._baseUrl}/articles/`, {
       method: "POST",
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify(article),
-    }).then((res) => this._checkResponse(res));
-  };
+    });
+  }
 
-  removeArticle = (articleId, token) => {
-    return fetch(`${this._baseUrl}/articles/${articleId}`, {
+  removeArticle(articleId, token) {
+    return this._request(`${this._baseUrl}/articles/${articleId}`, {
       method: "DELETE",
       headers: {
-        "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
       },
-    }).then((res) => this._checkResponse(res));
-  };
+    });
+  }
+
+  _request(url, options) {
+    options.headers = {
+      ...options.headers,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    };
+    return fetch(url, options).then((res) => this._checkResponse(res));
+  }
 
   _checkResponse(res) {
     if (res.ok) {
       return res.json();
     }
 
-    return res
-      .json()
-      .then((err) => {
-        return Promise.reject(`Error: ${res.status} - ${err.message}`);
-      })
-      .catch((err) => alert(err));
+    return res.json().then((err) => {
+      return Promise.reject(`Error: ${res.status} - ${err.message}`);
+    });
   }
 }
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -4,36 +4,34 @@ export const BASE_URL =
     : "http://localhost:3002";
 
 export const signin = ({ email, password }) => {
-  return fetch(`${BASE_URL}/signin`, {
+  return _request(`${BASE_URL}/signin`, {
     method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
     body: JSON.stringify({ email, password }),
-  }).then((res) => _checkReponse(res));
+  });
 };
 
 export const signup = ({ email, password, username }) => {
-  return fetch(`${BASE_URL}/signup`, {
+  return _request(`${BASE_URL}/signup`, {
     method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
     body: JSON.stringify({ email, password, username }),
-  }).then((res) => _checkReponse(res));
+  });
 };
 
-const _checkReponse = (res) => {
+const _request = (url, options) => {
+  options.headers = {
+    ...options.headers,
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  };
+  return fetch(url, options).then((res) => _checkResponse(res));
+};
+
+const _checkResponse = (res) => {
   if (res.ok) {
     return res.json();
   }
 
-  return res
-    .json()
-    .then((err) => {
-      return Promise.reject(`Error: ${res.status} - ${err.message}`);
-    })
-    .catch((err) => alert(err));
+  return res.json().then((err) => {
+    return Promise.reject(`Error: ${res.status} - ${err.message}`);
+  });
 };
